622d436b0719d7310fce6329303d62f92cddd297
qpenta <zzyagos1@gmail.com>
2024-03-16 01:04:05 -0300

    primeiro componente

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <PrimeiroComponente />
+  <h1>Olá mundo!</h1>
+</template>
+
+<script>
+import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+
+export default {
+  name: "App",
+  components: {
+    PrimeiroComponente,
+  },
+};
+</script>
