5b84ac21ece63f9e97dba2bb1f310ab8ee31b6ac
qpenta <zzyagos1@gmail.com>
2024-03-19 15:03:31 -0300

    feat: inserindo dados no banco

--- project/src/components/BurgerForm.vue
+++ project/src/components/BurgerForm.vue
@@ -2,7 +2,7 @@
   <div>
     <p>Componente de mensagem</p>
     <div>
-      <form id="burger-form">
+      <form id="burger-form" @submit="createBurger">
         <div class="input-container">
           <label for="nome">Nome do cliente:</label>
           <input
@@ -68,8 +68,7 @@ export default {
       nome: null,
       pao: null,
       carne: null,
-      opnionais: [],
-      status: "Solicitado",
+      opcionais: [],
       msg: null,
     };
   },
@@ -81,6 +80,37 @@ export default {
       this.paes = data.paes;
       this.carnes = data.carnes;
       this.opcionaisdata = data.opcionais;
+    },
+    async createBurger(e) {
+      e.preventDefault();
+
+      const data = {
+        nome: this.nome,
+        carne: this.carne,
+        pao: this.pao,
+        opcionais: Array.from(this.opcionais),
+        status: "Solicitado",
+      };
+      const dataJson = JSON.stringify(data);
+
+      const req = await fetch("http://localhost:3000/burgers", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: dataJson,
+      });
+
+      const res = await req.json();
+
+      //Mensagem de sistema
+      //Limpar mensagem da tela
+      //Limpar os campos
+
+      this.nome = "";
+      this.carne = "";
+      this.pao = "";
+      this.opcionais = "";
+
+      console.log(res);
     },
   },
   mounted() {
