f8879a84e0641ba327d613c4f05bad351a5d5905
qpenta <zzyagos1@gmail.com>
2024-03-20 13:04:02 -0300

    feat: finalizando crud no dashboard, faltando somente o update

--- project/src/components/TheDashboard.vue
+++ project/src/components/TheDashboard.vue
@@ -11,40 +11,34 @@
       </div>
     </div>
     <div id="burger-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>Nome</div>
-        <div>Pão de Trigo</div>
-        <div>Maminha</div>
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.nome }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.carne }}</div>
         <div>
           <ul>
-            <li>Salame</li>
+            <li v-for="(opcional, index) in burger.opcionais" :key="index">
+              {{ opcional }}
+            </li>
             <li>Tomate</li>
           </ul>
         </div>
         <div>
           <select name="status" class="status">
             <option value="">Selecione</option>
+            <option
+              v-for="s in status"
+              :key="s.id"
+              value="s.tipo"
+              :selected="burger.status == s.tipo"
+            >
+              {{ s.tipo }}
+            </option>
           </select>
-          <button class="delete-btn">Cancelar</button>
-        </div>
-      </div>
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>Nome</div>
-        <div>Pão de Trigo</div>
-        <div>Maminha</div>
-        <div>
-          <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
-          </ul>
-        </div>
-        <div>
-          <select name="status" id="status">
-            <option value="">Selecione</option>
-          </select>
-          <button class="delete-btn">Cancelar</button>
+          <button class="delete-btn" @click="deleteBurger(burger.id)">
+            Cancelar
+          </button>
         </div>
       </div>
     </div>
@@ -54,6 +48,51 @@
 <script>
 export default {
   name: "TheDashboard",
+  data() {
+    return {
+      burgers: null,
+      burger_id: null,
+      status: [],
+    };
+  },
+
+  methods: {
+    async getPedidos() {
+      const req = await fetch("http://localhost:3000/burgers");
+
+      const data = await req.json();
+
+      this.burgers = data;
+
+      //resgatar os status
+
+      this.getStatus();
+    },
+
+    async getStatus() {
+      const req = await fetch("http://localhost:3000/status");
+
+      const data = await req.json();
+
+      this.status = data;
+    },
+
+    async deleteBurger(id) {
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "DELETE",
+      });
+
+      // eslint-disable-next-line no-unused-vars
+      const res = await req.json();
+
+      //msg
+
+      this.getPedidos();
+    },
+  },
+  mounted() {
+    this.getPedidos();
+  },
 };
 </script>
 
